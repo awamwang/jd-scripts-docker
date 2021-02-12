45580900e378a8ba2fd19e169da64e0394e4bb5b
Awam M Wang <wm.keepgoing@gmail.com>
2021-02-12 20:48:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 if [ $# -gt 0 ]; then
-    docker-compose up --build --force-recreate -d $*
+    if [ -n "$1" ]; then
+        sed -i "s/pt_key=\S*/pt_key=${1}/g" env/env1
+        docker-compose up --build --force-recreate -d jd1
+    fi
+    if [ -n "$2" ]; then
+        sed -i "s/pt_key=\S*/pt_key=${2}/g" env/env2
+        docker-compose up --build --force-recreate -d jd2
+    fi
+    if [ -n "$3" ]; then
+        sed -i "s/pt_key=\S*/pt_key=${3}/g" env/env3
+        docker-compose up --build --force-recreate -d jd3
+    fi    
 else
     docker-compose up --build --force-recreate -d jd1
 fi
